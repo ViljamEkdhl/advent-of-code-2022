9eba062c807da0f7a1137bbf87253b315e33c025
William Ekdahl <viljam89@hotmail.com>
2022-12-01 15:58:32 +0100

    Day 1 - Star 1

---
+++ aoc-2022/Day1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Day_1
+{
+    public class ReturnInput
+    {
+        public static void dayOne()
+        {
+            var elves = new List<Elf>();
+            var data = System.IO.File.ReadAllLines(@"C:\projects\advent-of-code-2022\aoc-2022\input\Day1.txt");
+            var calories = 0;
+            var elf = 1;
+
+            foreach (var line in data)
+            {
+                if (int.TryParse(line, out int output))
+                {
+                    calories += int.Parse(line);
+                }
+                else
+                {
+                    elves.Add(new Elf(elf, calories));
+                    elf += 1;
+                    calories = 0;
+                }
+            }
+            elves.Add(new Elf(elf, calories));
+            
+            var c = 0;
+            var previous = 0;
+            foreach (var listItem in elves)
+            {
+                //Console.WriteLine("Id {0} Name {1}", listItem.ElfNr, listItem.Calories);
+                if (listItem.Calories <= previous) continue;
+                previous = listItem.Calories;
+                c = listItem.Calories;
+            }
+            Console.WriteLine("The correct answer is: " + c);
+        }
+    }
+}
---
+++ aoc-2022/Elf.cs
@@ -0,0 +1,14 @@
+﻿namespace Day_1
+{
+    public class Elf
+    {
+        internal readonly int ElfNr;
+        internal readonly int Calories;
+
+        public Elf(int elfNr, int calories)
+        {
+            this.ElfNr = elfNr;
+            this.Calories = calories;
+        }
+    }
+}
